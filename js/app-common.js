3d4eae91a62c248d321e60c2630505f047bbb843
uropiv <uropiv@gmail.com>
2025-11-27 13:03:19 -0300

    Update app-common.js

--- js/app-common.js
+++ js/app-common.js
@@ -1,6 +1,6 @@
 // js/app-common.js
 // Reemplaza la URL por tu Web App URL
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxEuDPHtsAupgFrO5I2JKugAr9QPRcDYhbS_JcB0Pruo4tMBJvZe7CiUJAoLKe437UG-w/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwsnM5ViHWzdrIg_hB1tRp7t6RVL9Lhq_UBpCYsucWDJyelIw8afoqBCbnxjQx1esBxNQ/exec";
 
 // Función que envía application/x-www-form-urlencoded para evitar CORS preflight
 async function api(action, payload = {}){
