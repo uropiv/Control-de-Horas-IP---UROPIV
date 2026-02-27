1c1bf5886c23c835f6caeade6d5613dc2e3fcdeb
uropiv <uropiv@gmail.com>
2026-02-27 15:28:05 -0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Configuracion central de la app.
+// Cambia solo esta URL cuando actualices el deploy de GAS.
+window.APP_CONFIG = {
+  GAS_URL: "https://script.google.com/macros/s/AKfycbwwfE8ofVXC2pGTTUwAaDsIY8fVwfsQ28WcdCWMh3gIQslKbzrJQ-c9MI0PLUbpg6ST8w/exec"
+};
