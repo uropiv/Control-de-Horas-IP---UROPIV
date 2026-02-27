937493742fd05ee8b45cc6d12dd7e97fc1bb7976
uropiv <uropiv@gmail.com>
2026-02-27 14:40:00 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// Configuracion central de la app.
-// Cambia solo esta URL cuando actualices el deploy de GAS.
-window.APP_CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbxLH44JRsNXkYVMuxI4kXOzVz8qaTDmg1o6MZXu4NFB2WiV1ZRQ8xu2xenOE0UvdAss4w/exec"
-};
